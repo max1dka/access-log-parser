b26f95254365a74b53e634a8e83a8016ddafb76a
MPMitrofanov <mitrofanov.m00@icloud.com>
2025-11-26 23:50:03 +0300

    Новая ветка + новая программа с арифметическими действиями

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in) .nextLine() ;
-        System.out.println(("Длина текста: " + text.length()));
-    }
+        System.out.println("Введите первое число:");
+        int number = new Scanner(System.in) .nextInt();
+        System.out.println("Введите второе число:");
+        int second_number = new Scanner(System.in) .nextInt();
+        int summ = number + second_number;
+        int diff = number - second_number;
+        int umn = number * second_number;
+        double delenie = (double) number / second_number;
+        System.out.println( "Сумма чисел равна " + summ);
+        System.out.println( "Разность чисел равна " + diff);
+        System.out.println( "Умножение чисел равно " + umn);
+        System.out.println( "Деление чисел равно " + delenie);
+      }
 }
