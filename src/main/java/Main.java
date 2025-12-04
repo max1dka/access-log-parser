576ecd213cece79083559a1de4d5abfa680b29fb
MPMitrofanov <mitrofanov.m00@icloud.com>
2025-12-04 17:34:37 +0300

    Изменения: 1) Добавлен ввод пути файла; 2) Добавлена проверка, не указана ли папка; 4) Добавлена проверка, существует ли файл; 5) Программа каждый раз запрашивает новый путь; 6) Если файл существует в директории + обновляется и выводится счетчик

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in) .nextLine() ;
-        System.out.println(("Длина текста: " + text.length()));
+        int ways = 0;
+        while (true) {
+        String path = new Scanner(System.in).nextLine();
+        File file = new File(path);
+        boolean isDirectory = file.isDirectory();
+        boolean fileExists = file.exists();
+            System.out.println(isDirectory);
+            System.out.println(fileExists);
+        if (isDirectory) {
+            System.out.println("Данный адрес не ведет к файлу (вы указали путь к папке) ");
+            continue;
+        }
+
+        if (fileExists == false) {
+            System.out.println("Указаного файла не существует или введено некорректное значение");
+            continue;
+        }
+
+        ways += + 1;
+        System.out.println("Путь указан верно! Файл существует!");
+        System.out.println("Верно указанных путей: " + ways);
+
+        }
+
     }
 }
