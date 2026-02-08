2277e80227fc9194aaab4b31c5d4288681ad049e
max1dka <mitrofanov.m00@icloud.com>
2026-02-08 19:25:08 +0300

    + Добавлено чтение файла; + Добавлен вывод самой короткой / длинной строки и общее кол-во строк; + Добавлена проверка на длину строки; + Добавлен exception с пояснением

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LineTooLongException {
         int ways = 0;
         while (true) {
         String path = new Scanner(System.in).nextLine();
@@ -24,8 +26,38 @@ public class Main {
         ways += 1;
         System.out.println("Путь указан верно! Файл существует!");
         System.out.println("Верно указанных путей: " + ways);
+            int CounterLines = 0;
+            int min = Integer.MAX_VALUE;
+            int max = 0;
 
+           try {
+               FileReader fileReader = new FileReader(file);
+            BufferedReader reader =
+                    new BufferedReader(fileReader);
+            String line;
+            while ((line = reader.readLine()) != null){
+                CounterLines++;
+                int lenght = line.length();
+                if (lenght > 1024) {
+                    throw new LineTooLongException("Обнаружена строка длиннее 1024 символов (длина: " + lenght + ")");
+                }
+                if (lenght > max ){
+                    max = lenght;
+                }
+                if (lenght < min){
+                    min = lenght;
+                }
+            }   System.out.println("Длина самой короткой строки:" + min);
+                System.out.println("Длина самой длинной строки:" + max);
+                System.out.println("Общее количество строк в файле: " + CounterLines);
+
+
+           } // C:\Users\max23\OneDrive\Рабочий стол\access.log
+           catch (Exception ex) {
+               ex.printStackTrace();
+           }
         }
+
 
     }
 }
